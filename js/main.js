10dd0b2021a4dd09954e38b7284c318ad4420acf
adryanzx <adryan_freire@hotmail.com>
2023-06-02 17:46:05 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let ul = document.querySelector('nav .ul');
+
+function openMenu(){
+    ul.classList.add('open');
+}
+
+function closeMenu(){
+    ul.classList.remove('open');
+}
+const swiper = new Swiper('.carousel', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    }
+})
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.banner-text', {duration:1000});
+
+sr.reveal('.php-text', {duration:1000});
+
+sr.reveal('.php-img', {duration:1000});
+
+sr.reveal('.conectar', {duration:1000});
+
+sr.reveal('.card', {duration:1000});
+
+sr.reveal('.informacoes', {duration:1000});
+
+sr.reveal('.cards', {duration:1000});
+
+
+
